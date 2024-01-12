d58952062a59a0a1d6a0326e1857cf7e1038c3bd
pablonv811 <pablonv811@gmail.com>
2024-01-12 12:22:39 +0100

    Proyecto creado

---
+++ p5/src/main/java/com/mycompany/p5/P5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p5;
+
+/**
+ *
+ * @author pablo
+ */
+public class P5 {
+
+    public static void main(String[] args) {
+        System.out.println("me cago en los muertos del jesus ca nonigo");
+    }
+}
